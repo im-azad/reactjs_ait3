00bc06e9499e8ca323e253418a5ee340dfe2c026
Abul Kalam Azad <me.azad99@gmail.com>
2022-05-16 16:11:15 +0600

    What is AIT-3 section add

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = () => {
-	return <div>Feature</div>;
+const Feature = ({ title, text }) => {
+	return (
+		<div className='ait3__features-container__single-feature'>
+			<div className='ait3__features-container__feature-title'>
+				<div />
+				<h1>{title}</h1>
+			</div>
+			<div className='ait3__features-container__feature-text'>{text}</div>
+		</div>
+	);
 };
 
 export default Feature;
--- src/containers/whatAIT3/WhatAIT3.jsx
+++ src/containers/whatAIT3/WhatAIT3.jsx
@@ -6,18 +6,32 @@ const WhatAIT3 = () => {
 	return (
 		<div className='ait3__whatait3 section__margin'>
 			<div className='ait3__whitait3-feature'>
-				<Feature />
+				<Feature
+					title='What is AIT-3'
+					text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'
+				/>
 			</div>
 			<div className='ait3__whatait3-heading'>
-				<h1 className='gradient-text'>
+				<h1 className='gradient__text'>
 					The possibilities are beyond your imagination
 				</h1>
-				<p>Explore The Library</p>
+				<p>
+					<a href='index.html'>Explore The Library</a>
+				</p>
 			</div>
 			<div className='ait3__whatait3__feature-container'>
-				<Feature />
-				<Feature />
-				<Feature />
+				<Feature
+					title='Chatbots'
+					text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. Always open Whole front do of plate heard oh ought.'
+				/>
+				<Feature
+					title='Knowledgebase'
+					text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments.We so opinion friends me message as delight. '
+				/>
+				<Feature
+					title='Education'
+					text='At jointure ladyship an insisted so humanity he. We so opinion friends me message as delight. Friendly bachelor entrance to on by. As put impossible own apartments.'
+				/>
 			</div>
 		</div>
 	);
