a224e6a1a3c2739ad91a8db440b6d46c0cc44abb
Abul Kalam Azad <me.azad99@gmail.com>
2022-05-17 10:30:13 +0600

    CTA section add

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import './cta.css';
 
 const CTA = () => {
-	return <div>CTA</div>;
+	return (
+		<div>
+			<div className='ait3__cta'>
+				<div className='ait3__cta-content'>
+					<p>Request Early Access to Get Started</p>
+					<h3>
+						Register today & start exploring the endless
+						possiblities.
+					</h3>
+				</div>
+				<div className='ait3__cta-button'>
+					<button type='button'>Get Started</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CTA;
